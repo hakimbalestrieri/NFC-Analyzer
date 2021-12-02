0001ef2631236ccc56cc5dead789799a03db1036
Alexis Allemann <alexis.allemann@gmail.com>
2021-12-02 21:23:43 +0100

    Commencé NFC

--- app/src/main/java/ch/heigvd/symlabo3/nfc/NFCActivity.kt
+++ app/src/main/java/ch/heigvd/symlabo3/nfc/NFCActivity.kt
@@ -1,12 +1,37 @@
 package ch.heigvd.symlabo3.nfc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import ch.heigvd.symlabo3.R
+import ch.heigvd.symlabo3.databinding.ActivityNfcactivityBinding
 
 class NFCActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNfcactivityBinding
+    private lateinit var mNfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfcactivity)
+
+        // Binding components
+        binding = ActivityNfcactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+
+        if (mNfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC enabled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "NFC disabled", Toast.LENGTH_LONG).show()
+        }
     }
 }
